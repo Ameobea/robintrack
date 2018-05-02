287c3562db64dbe4486c3e7a8a26d05eca267322
Casey Primozic <me@ameo.link>
2018-05-02 06:07:41 -0500

    Implement remaining API methods
     * Create Mongo query functions, formatters, and API routes for them

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :most_popular, to: "stocks#most_popular", defaults: { format: :json }
   get :least_popular, to: "stocks#least_popular", defaults: { format: :json }
+  get :quotes, to: "stocks#quotes", defaults: { format: :json }
   resources :stocks, only: [], defaults: { format: :json } do
     member do
       get :quote
