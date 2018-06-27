610979da2c1fd476fe96726cc3834d1cb746ff16
Casey Primozic <me@ameo.link>
2018-06-27 02:50:04 -0500

    Handle lowercase symbols provided in URLs
     * Add a check to the `PageTracker` which redirects to the capitalized version

--- frontend/src/components/PageTracker.js
+++ frontend/src/components/PageTracker.js
@@ -5,13 +5,54 @@
 
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+
+const symbolPageRegex = /\/symbol\/(.+)/;
+
+/**
+ * If we're currently on a symbol details page, check to see if the symbol is lowercase.  If it
+ * is, push a new path where the symbol is capitalized instead.
+ */
+const checkSymbolPage = path => {
+  const match = symbolPageRegex.exec(path);
+  if (!match) {
+    return false;
+  }
+
+  const uppercaseSymbol = match[1].toUpperCase();
+  if (uppercaseSymbol !== match[1]) {
+    return uppercaseSymbol;
+  }
+};
+
+const sendGa = curPath => {
+  window.ga('set', 'page', curPath);
+  window.ga('send', 'pageview');
+};
 
 class PageTracker extends Component {
+  checkLowercaseSymbol = () => {
+    const { path: curPath, push } = this.props;
+
+    const newPath = checkSymbolPage(curPath);
+    if (newPath) {
+      push(newPath);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  componentDidMount = this.checkLowercaseSymbol;
+
   componentDidUpdate = prevProps => {
     const curPath = this.props.path;
+    if (this.checkLowercaseSymbol()) {
+      return;
+    }
+
     if (prevProps.path !== this.props.path) {
-      window.ga('set', 'page', curPath);
-      window.ga('send', 'pageview');
+      window.ga && sendGa(curPath);
     }
   };
 
@@ -24,4 +65,7 @@ const mapStateToProps = ({
   },
 }) => ({ path: pathname });
 
-export default connect(mapStateToProps)(PageTracker);
+export default connect(
+  mapStateToProps,
+  { push }
+)(PageTracker);
