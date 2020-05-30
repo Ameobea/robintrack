78962c5a7c797dde9dc07e664a8da8d0401b161b
Alex J <2790993+dalexj@users.noreply.github.com>
2020-05-30 16:46:57 -0600

    use rate limiter in application controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   class NotFound < StandardError; end
   class BadRequest < StandardError; end
 
+  before_action :rate_limit_by_ip
+
+  def rate_limit_by_ip
+    if RateLimiter.should_block_request?(request.remote_ip)
+      render status: :too_many_requests
+    else
+      RateLimiter.incr_requests(request.remote_ip)
+    end
+  end
+
   rescue_from NotFound do
     render json: { error: "not_found" }, status: :not_found
   end
