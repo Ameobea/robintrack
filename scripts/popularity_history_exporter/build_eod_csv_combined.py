3d85772acc7febb14b9fb9e2242a62f8f3e12371
Casey Primozic <me@ameo.link>
2020-06-15 23:56:33 -0700

    Add combined EOD CSV to daily export script

---
+++ scripts/popularity_history_exporter/build_eod_csv_combined.py
@@ -0,0 +1,39 @@
+import os
+
+import click
+
+@click.command()
+@click.argument("output-directory", type=click.Path())
+def main(output_directory: str):
+  out = open(os.path.join(output_directory, "combined.csv"), "w")
+  out.write("date,asset,popularity\n")
+
+  for fname in os.listdir(output_directory):
+    if not ".csv" in fname:
+      continue
+    elif fname.startswith("combined"):
+      continue
+
+    symbol = fname.split(".csv")[0]
+    if symbol.startswith("_"):
+      symbol = symbol[1:]
+    with open(fname) as f:
+      next(f) # skip header row
+
+      last_date = None
+      for line in f:
+        date = line[1:11]
+        if date == last_date:
+          continue
+
+        last_date = date
+        popularity = line.split('",')[1]
+
+        out.write(date)
+        out.write(",")
+        out.write(symbol)
+        out.write(",")
+        out.write(popularity)
+
+if __name__ == "__main__":
+  main() # pylint: disable=no-value-for-parameter
--- scripts/popularity_history_exporter/run_exporter.sh
+++ scripts/popularity_history_exporter/run_exporter.sh
@@ -5,3 +5,4 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 python main.py $1
+python build_eod_csv_combined.py $1
